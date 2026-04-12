22516a9b06fb8a42134ce67d2db6b6ceb1dd0b16
Osvaldo Santillan <osvaldonuevo@gmail.com>
2026-04-11 23:11:35 -0600

    feat: enhance product entity and service with slug handling and error management

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { error } from 'console';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger=new Logger('ProductsService');  
 
   constructor(
     @InjectRepository(Product)
@@ -18,30 +21,76 @@ export class ProductsService {
   async create(createProductDto: CreateProductDto) {
      
     try{
+
+      // if(!createProductDto.slug){
+      //   createProductDto.slug=createProductDto.title.toLowerCase().replaceAll(' ', '-').replaceAll("'", '');
+      // }else{
+      //   createProductDto.slug=createProductDto.slug.toLowerCase().replaceAll(' ', '-').replaceAll("'", '');
+      // }
+
       const product =this.productRepository.create(createProductDto);
       await this.productRepository.save(product);
       return product;
-  }catch(error){
-    console.log(error);
-    throw new InternalServerErrorException('Error creating product');
+  }catch(error:any){
+
+    this.handleExceptions(error);
+   
   }
 
 
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find()
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string){
+    try{
+        const product=await this.productRepository.findOneBy({id:id.toString()})
+        if(!product) {
+        const error:any=new Error(`Product with id ${id} not found`)
+        error.code='404'
+        throw error
+      }
+        return product;
+    }catch(error:any){
+        this.handleExceptions(error)
+    }
+  
+
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+   async remove(id: string) {
+
+    await this.findOne(id)
+
+    await this.productRepository.delete(id);
+    
+   
+
+    
+
+    return `Product with id ${id} has been removed`;
   }
+
+  private handleExceptions(error:any){
+
+    switch(error.code){
+      case '23505':
+        throw new BadRequestException(error.detail); break
+      case '404':
+        throw new NotFoundException(error.message); break
+
+      default:
+        this.logger.error(error)
+        throw new InternalServerErrorException('Unexpected error, check server logs')
+    }
+
+  }
+
 }
